1848ade2345e8b87be9060b97d88a7dbab660587
ataerena <ataerena599@gmail.com>
2024-05-30 16:57:38 +0300

    completely changed the gameobject

--- Assets/Atas Folder/Scripts/AcousticFlare.cs
+++ Assets/Atas Folder/Scripts/AcousticFlare.cs
@@ -15,10 +15,10 @@ using Image = UnityEngine.UI.Image;
 public class AcousticFlare : MonoBehaviour
 {
     [SerializeField]
-    private Image markerImage;
+    private GameObject marker;
 
     [SerializeField]
-    private Image bannerImage;
+    private RawImage bannerImage;
 
     [SerializeField]
     Text ownerName;
@@ -33,8 +33,6 @@ public class AcousticFlare : MonoBehaviour
 
     private Transform uiTransform;
     private Human _human; // gonna use this for distance calculation
-    [SerializeField]
-    Canvas _canvas;
     Camera _camera;
 
     private float minX;
@@ -69,10 +67,9 @@ public class AcousticFlare : MonoBehaviour
 
         _human = PhotonExtensions.GetMyHuman().gameObject.GetComponent<Human>();
         _camera = FindFirstObjectByType<Camera>();
-        _canvas.worldCamera = _camera;
 
-        minX = markerImage.GetPixelAdjustedRect().width / 2;
-        minY = markerImage.GetPixelAdjustedRect().width / 2;
+        minX = 0;
+        minY = 0;
         ownerName.text = _player.GetStringProperty(PlayerProperty.Name);
         bannerImage.color = GenerateRandomColor();
 
@@ -85,7 +82,7 @@ public class AcousticFlare : MonoBehaviour
 
     private void ChangeCanvasLocation() // debugging was worthless so pushing this again to see if it fixes anything with a pull //
     {
-        Vector2 pos = _camera.WorldToViewportPoint(uiTransform.position);
+        Vector2 pos = _camera.WorldToScreenPoint(uiTransform.position);
 
         if (Vector3.Dot((uiTransform.position - _camera.transform.position), _camera.transform.forward) < 0)
         {
@@ -99,20 +96,13 @@ public class AcousticFlare : MonoBehaviour
             }
         }
 
-        pos.x = Mathf.Clamp(pos.x * Screen.width, minX, Screen.width - minX);
-        pos.y = Mathf.Clamp(pos.y * Screen.height, minY, Screen.height - minY);
+        pos.x = Mathf.Clamp(pos.x, minX, Screen.width - minX);
+        pos.y = Mathf.Clamp(pos.y, minY, Screen.height - minY);
 
-        markerImage.transform.position = pos;
-        bannerImage.transform.position = new Vector2(pos.x, pos.y - _offset1);
-        ownerName.transform.position = new Vector2(pos.x, pos.y - _offset1);
-        distance.transform.position = new Vector2(pos.x, pos.y - _offset2);
+        marker.transform.position = pos;
+
+        
         distance.text = "-" + ((int)Vector3.Distance(uiTransform.position, _human.transform.position)).ToString() + "U-";
-
-        Debug.Log("_camera: " + _camera.pixelWidth);
-        Debug.Log("pos " + pos);
-        Debug.Log("Marker Location: " + uiTransform.position);
-        Debug.Log("minX: " + minX);
-        Debug.Log("minY: " + minY);
     }
 
     private void ScaleUIElements() // optionally, add scaling for when player is too close (looks fine to me for now)
@@ -123,14 +113,7 @@ public class AcousticFlare : MonoBehaviour
 
         float scale = 250f / _distanceValue;
 
-        markerImage.transform.localScale = new Vector2( Mathf.Clamp(scale, 0.25f, 1f), Mathf.Clamp(scale, 0.25f, 1f));
-        bannerImage.transform.localScale = new Vector2( Mathf.Clamp(scale, 0.25f, 1f), Mathf.Clamp(scale, 0.25f, 1f));
-        
-        ownerName.fontSize = (int)(Mathf.Clamp(scale, .25f, 1f) * 30);
-        distance.fontSize = (int)(Mathf.Clamp(scale, .25f, 1f) * 20);
-
-        _offset1 = Mathf.Clamp(scale, .25f, 1f) * offset1;
-        _offset2 = Mathf.Clamp(scale, .25f, 1f) * offset2;
+        marker.transform.localScale = new Vector2( Mathf.Clamp(scale, 0.25f, 1f), Mathf.Clamp(scale, 0.25f, 1f));
     }
 
     private Color GenerateRandomColor()
