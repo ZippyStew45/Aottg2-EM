e2b7861c6e1580d04603d1202966423434900590
ataerena <ataerena599@gmail.com>
2024-05-31 17:21:23 +0300

    added late client rpc sync

--- Assets/Atas Folder/Scripts/AcousticFlare.cs
+++ Assets/Atas Folder/Scripts/AcousticFlare.cs
@@ -12,7 +12,7 @@ using UnityEngine.UI;
 using UnityEngine.UIElements;
 using Image = UnityEngine.UI.Image;
 
-public class AcousticFlare : MonoBehaviour
+public class AcousticFlare : MonoBehaviourPun, IPunObservable
 {
     [SerializeField]
     private GameObject marker;
@@ -34,7 +34,6 @@ public class AcousticFlare : MonoBehaviour
     private AudioSource flareSound;
 
     private Transform uiTransform;
-    private Human _human; // gonna use this for distance calculation
 
     private float minX;
     private float minY;
@@ -43,11 +42,13 @@ public class AcousticFlare : MonoBehaviour
     public void Setup(Transform _transform, Player _player)
     {
         PhotonView photonView = GetComponent<PhotonView>();
-        photonView.RPC("SetupRPC", RpcTarget.All, new object[] { _transform.position, _transform.rotation, _transform.localScale, _player });
+        Color _color = GenerateRandomColor();
+        if (photonView.IsMine)
+            photonView.RPC("SetupRPC", RpcTarget.AllBuffered, new object[] { _transform.position, _transform.rotation, _transform.localScale, _player, _color });
     }
 
     [PunRPC]
-    private void SetupRPC(Vector3 _position, Quaternion _rotation, Vector3 _scale, Player _player, PhotonMessageInfo info)
+    private void SetupRPC(Vector3 _position, Quaternion _rotation, Vector3 _scale, Player _player, Color _color, PhotonMessageInfo info)
     {
         uiTransform = gameObject.GetComponent<Transform>();
 
@@ -55,22 +56,59 @@ public class AcousticFlare : MonoBehaviour
         uiTransform.transform.rotation = _rotation;
         uiTransform.transform.localScale = _scale;
 
-        _human = PhotonExtensions.GetMyHuman().gameObject.GetComponent<Human>();
 
         minX = 0;
         minY = 0;
         ownerName.text = _player.GetStringProperty(PlayerProperty.Name);
         ownerName.text = RemoveColorTagsFromName(ownerName.text);
-        bannerImage.color = GenerateRandomColor();
+        bannerImage.color = _color;
 
         flareSound.Play();
-        if ((int)Vector3.Distance(uiTransform.position, _human.transform.position) < 250)
+        
+        if ((int)Vector3.Distance(uiTransform.position, SceneLoader.CurrentCamera.Camera.transform.position) < 250)
         {
             ringingSound.Play();
         }
     }
 
-    private void ChangeCanvasLocation() // debugging was worthless so pushing this again to see if it fixes anything with a pull //
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+{
+    if (stream.IsWriting)
+    {
+        stream.SendNext(this.uiTransform.position);
+        stream.SendNext(this.uiTransform.rotation);
+        stream.SendNext(this.uiTransform.localScale);
+        stream.SendNext(new Vector4(bannerImage.color.r, bannerImage.color.g, bannerImage.color.b, bannerImage.color.a));
+        stream.SendNext(this.ownerName.text);
+        stream.SendNext(this.distance != null ? this.distance.text : "");
+    }
+    else
+    {
+        uiTransform.position = (Vector3)stream.ReceiveNext();
+        uiTransform.rotation = (Quaternion)stream.ReceiveNext();
+        uiTransform.localScale = (Vector3)stream.ReceiveNext();
+
+        Vector4 colorVector = (Vector4)stream.ReceiveNext();
+        bannerImage.color = new Color(colorVector.x, colorVector.y, colorVector.z, colorVector.w);
+
+        ownerName.text = (string)stream.ReceiveNext();
+        if (distance != null)
+        {
+            distance.text = (string)stream.ReceiveNext();
+        }
+        else
+        {
+            Debug.LogWarning("Distance text component is null");
+        }
+
+        ownerName.text = RemoveColorTagsFromName(ownerName.text);
+
+        minX = 0;
+        minY = 0;
+    }
+}
+
+    private void ChangeCanvasLocation()
     {
         Vector3 pos = SceneLoader.CurrentCamera.Camera.WorldToViewportPoint(uiTransform.position);
 
@@ -94,13 +132,13 @@ public class AcousticFlare : MonoBehaviour
 
         marker.transform.position = pos;
 
-        
-        distance.text = "-" + ((int)Vector3.Distance(uiTransform.position, _human.transform.position)).ToString() + "U-";
+        if (SceneLoader.CurrentCamera.Camera != null && distance != null)
+            distance.text = "-" + ((int)Vector3.Distance(uiTransform.position, SceneLoader.CurrentCamera.Camera.transform.position)).ToString() + "U-";
     }
 
-    private void ScaleUIElements() // optionally, add scaling for when player is too close (looks fine to me for now)
+    private void ScaleUIElements()
     {
-        float _distanceValue = Vector3.Distance(uiTransform.position, _human.transform.position);
+        float _distanceValue = Vector3.Distance(uiTransform.position, SceneLoader.CurrentCamera.Camera.transform.position);
 
         if (_distanceValue > 40f && _distanceValue < 10000f)
         {
@@ -113,7 +151,7 @@ public class AcousticFlare : MonoBehaviour
 
     private void ScaleOpacity()
     {
-        float _distanceValue = Vector3.Distance(uiTransform.position, _human.transform.position);
+        float _distanceValue = Vector3.Distance(uiTransform.position, SceneLoader.CurrentCamera.Camera.transform.position);
         if (_distanceValue > 40f && _distanceValue <= 10000f)
         {
             float scale = _distanceValue / 600f;
@@ -173,7 +211,7 @@ public class AcousticFlare : MonoBehaviour
         if (timeLeft <= 0)
             DestorySelf();
 
-        if ( SceneLoader.CurrentCamera.Camera != null && uiTransform != null)
+        if ( SceneLoader.CurrentCamera.Camera != null && uiTransform != null )
         {
             ChangeCanvasLocation();
             ScaleUIElements();
